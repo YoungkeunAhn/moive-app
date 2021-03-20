48e0b9e71ceb0f2eae64368fbd082b26db7af948
YoungRoot <tosan2094@gmail.com>
2021-03-20 13:37:13 +0900

    Study axios

--- src/App.js
+++ src/App.js
@@ -1,47 +1,29 @@
 import React from 'react';
-// import propTypes from 'prop-types';
+import axios from 'axios';
  
 
 class App extends React.Component{
 
   state ={
-    count: 0,
+    isLoading: true,
+    movie: [],
   }
 
-  plus = () =>{
-    console.log("plus");
-    this.setState(current => ({ count : current.count + 1 }));
-    //current를 사용해서 this.state.count를 접근하는 것이 아니라 current로 state를 불러와서 사용하는게 더욱 좋은 코드
+  getMovies =  async () =>{
+    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
   }
+  
 
-  minus = () =>{
-    console.log("minus");
-    this.setState(current => ({ count : current.count - 1 }));
-  }
-
-  //React Life Cycle
-  //Mounting - 컴포넌트가 생성될 때
   componentDidMount(){
-    console.log("rendered");
-  }
-
-  //Updating - setState()가 호출될 떄
-  componentDidUpdate(){
-    console.log("Updated");
-  }
-
-  //Unmounting - 컴포넌트가 죽을 때, 다른 컴포넌트로 바뀔 때, 페이지가 이동할 때
-  componentWillUnmount(){
-    console.log("Unmount")
+    this.getMovies();
+    // this.setState({isLoading : false});
   }
 
   render(){
-    console.log("I'm rendering!!")
+    const { isLoading } = this.state;
     return(
       <div>
-        <h1>Number : { this.state.count }</h1>
-        <button onClick={this.plus}>plus</button>
-        <button onClick={this.minus}>minus</button>
+        { isLoading ? "Loading..." : "We are ready" }
       </div>
     )
   }
